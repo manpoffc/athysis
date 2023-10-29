68c7a95b3e73884ec14801c94700c12780273229
Manpreet Singh <manpoffc@gmail.com>
2023-10-29 14:30:44 -0400

    filer

--- app/components/FilterDropDown.tsx
+++ app/components/FilterDropDown.tsx
@@ -1,6 +1,6 @@
 import { FaBeer } from "react-icons/fa";
 import { FaArrowDown, FaArrowUp } from "react-icons/fa";
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 type Props = {};
 interface Athlete {
@@ -16,166 +16,182 @@ interface Athlete {
   sport: string;
 }
 interface FilterDropDownProps {
-  fetchData: (data: any) => void
+  fetchData: (data: any) => void;
+  filterData: any;
+  setFilterData: (data: any) => void;
 }
-const FilterDropDown: React.FC<FilterDropDownProps> = ({ fetchData }) => {
+const FilterDropDown: React.FC<FilterDropDownProps> = ({
+  fetchData,
+  filterData,
+  setFilterData,
+}) => {
   const [isDropdownOpen, setDropdownOpen] = useState(false);
-  const [filteredData, setFilteredData] = useState({
-    ageRange: [18, 65],
-    gender: 'all',
-    citizen: 'all'
-  });
 
   const toggleDropdown = () => {
     setDropdownOpen(!isDropdownOpen);
   };
 
   const resetFilters = () => {
-    setFilteredData({
+    setFilterData({
       ageRange: [18, 65],
-      gender: 'all',
-      citizen: 'all'
-    })
+      gender: "all",
+      citizen: "all",
+    });
     fetchData({
       ageRange: [18, 65],
-      gender: 'all',
-      citizen: 'all'
-    })
+      gender: "all",
+      citizen: "all",
+    });
   };
   const handleCitizenChange = (data: string) => {
-
-    setFilteredData({ ...filteredData, citizen: data })
-  }
+    setFilterData({ ...filterData, citizen: data });
+  };
 
   const handleGenderChange = (data: any) => {
-
-    setFilteredData({ ...filteredData, gender: data })
-
-  }
+    setFilterData({ ...filterData, gender: data });
+  };
   const handleMinAgeChange = (value: number) => {
-    const newMinAge = Math.min(value, filteredData.ageRange[1]);
-    setFilteredData({ ...filteredData, ageRange: [newMinAge, filteredData.ageRange[1]] });
+    const newMinAge = Math.min(value, filterData.ageRange[1]);
+    setFilterData({
+      ...filterData,
+      ageRange: [newMinAge, filterData.ageRange[1]],
+    });
   };
-  
+
   const handleMaxAgeChange = (value: number) => {
-    const newMaxAge = Math.max(value, filteredData.ageRange[0]);
-    setFilteredData({ ...filteredData, ageRange: [filteredData.ageRange[0], newMaxAge] });
+    const newMaxAge = Math.max(value, filterData.ageRange[0]);
+    setFilterData({
+      ...filterData,
+      ageRange: [filterData.ageRange[0], newMaxAge],
+    });
   };
-  
+
   return (
-    <div className='relative p-2'>
+    <div className="relative p-2">
       <button
-        className='bg-cyan-700 flex justify-center items-center space-x-5 text-white w-28 h-10 rounded-md'
+        className="bg-cyan-700 flex justify-center items-center space-x-5 text-white w-28 h-10 rounded-md"
         onClick={toggleDropdown}
       >
         <span>Filter</span> {isDropdownOpen ? <FaArrowUp /> : <FaArrowDown />}
       </button>
       {isDropdownOpen && (
-        <div className='absolute mt-2 p-8 w-72 bg-white border rounded-md shadow-md z-10'>
-          <div className='mb-4'>
-            <label className='block font-medium mb-2'>Age</label>
+        <div className="absolute mt-2 p-8 w-72 bg-white border rounded-md shadow-md z-10">
+          <div className="mb-4">
+            <label className="block font-medium mb-2">Age</label>
             <input
-              type='range'
-              min='0'
-              max='100'
-              step='1'
-              value={filteredData.ageRange[0]}
+              type="range"
+              min="0"
+              max="100"
+              step="1"
+              value={filterData.ageRange[0]}
               onChange={(e) => handleMinAgeChange(parseInt(e.target.value))}
-              className='w-full'
+              className="w-full"
             />
             <input
-              type='range'
-              min='0'
-              max='100'
-              step='1'
-              value={filteredData.ageRange[1]}
+              type="range"
+              min="0"
+              max="100"
+              step="1"
+              value={filterData.ageRange[1]}
               onChange={(e) => handleMaxAgeChange(parseInt(e.target.value))}
-              className='w-full'
+              className="w-full"
             />
-            <p className='text-center'>
-              Age: {filteredData.ageRange[0]} - {filteredData.ageRange[1]}
+            <p className="text-center">
+              Age: {filterData.ageRange[0]} - {filterData.ageRange[1]}
             </p>
           </div>
-          <div className='mb-4'>
-            <label className='block font-medium mb-2'>Gender</label>
-            <div className='flex text-center justify-between'>
-              <label className='mr-2 flex flex-col'>
+          <div className="mb-4">
+            <label className="block font-medium mb-2">Gender</label>
+            <div className="flex text-center justify-between">
+              <label className="mr-2 flex flex-col">
                 <input
-                  type='radio'
-                  name='gender'
-                  value='all'
-                  checked={filteredData.gender === 'all'}
-                  onChange={() => { handleGenderChange('all') }}
+                  type="radio"
+                  name="gender"
+                  value="all"
+                  checked={filterData.gender === "all"}
+                  onChange={() => {
+                    handleGenderChange("all");
+                  }}
                 />
                 All
               </label>
-              <label className='mr-2 flex flex-col'>
+              <label className="mr-2 flex flex-col">
                 <input
-                  type='radio'
-                  name='gender'
-                  value='male'
-                  checked={filteredData.gender === 'male'}
-                  onChange={() => { handleGenderChange('male') }}
+                  type="radio"
+                  name="gender"
+                  value="male"
+                  checked={filterData.gender === "male"}
+                  onChange={() => {
+                    handleGenderChange("male");
+                  }}
                 />
                 Male
               </label>
-              <label className='flex flex-col'>
+              <label className="flex flex-col">
                 <input
-                  type='radio'
-                  name='gender'
-                  value='female'
-                  checked={filteredData.gender === 'female'}
-                  onChange={() => { handleGenderChange('female') }}
+                  type="radio"
+                  name="gender"
+                  value="female"
+                  checked={filterData.gender === "female"}
+                  onChange={() => {
+                    handleGenderChange("female");
+                  }}
                 />
                 Female
               </label>
             </div>
           </div>
-          <div className='mb-4'>
-            <label className='block font-medium mb-2'>Citizen</label>
-            <div className='flex justify-between'>
-              <label className='mr-2 flex flex-col'>
+          <div className="mb-4">
+            <label className="block font-medium mb-2">Citizen</label>
+            <div className="flex justify-between">
+              <label className="mr-2 flex flex-col">
                 <input
-                  type='radio'
-                  name='citizen'
-                  value='all'
-                  checked={filteredData.citizen === 'all'}
-                  onChange={() => { handleCitizenChange('all') }}
+                  type="radio"
+                  name="citizen"
+                  value="all"
+                  checked={filterData.citizen === "all"}
+                  onChange={() => {
+                    handleCitizenChange("all");
+                  }}
                 />
                 All
               </label>
-              <label className='mr-2 flex flex-col'>
+              <label className="mr-2 flex flex-col">
                 <input
-                  type='radio'
-                  name='citizen'
-                  value='yes'
-                  checked={filteredData.citizen === 'yes'}
-                  onChange={() => { handleCitizenChange('yes') }}
+                  type="radio"
+                  name="citizen"
+                  value="yes"
+                  checked={filterData.citizen === "yes"}
+                  onChange={() => {
+                    handleCitizenChange("yes");
+                  }}
                 />
                 Yes
               </label>
-              <label className='flex flex-col'>
+              <label className="flex flex-col">
                 <input
-                  type='radio'
-                  name='citizen'
-                  value='no'
-                  checked={filteredData.citizen === 'no'}
-                  onChange={() => { handleCitizenChange('no') }}
+                  type="radio"
+                  name="citizen"
+                  value="no"
+                  checked={filterData.citizen === "no"}
+                  onChange={() => {
+                    handleCitizenChange("no");
+                  }}
                 />
                 No
               </label>
             </div>
           </div>
-          <div className='flex justify-between'>
+          <div className="flex justify-between">
             <button
-              className='bg-red-500 text-white w-20 h-8 rounded-md'
+              className="bg-red-500 text-white w-20 h-8 rounded-md"
               onClick={resetFilters}
             >
               Reset
             </button>
             <button
-              className='bg-green-500 text-white w-20 h-8 rounded-md'
-              onClick={() => fetchData(filteredData)}
+              className="bg-green-500 text-white w-20 h-8 rounded-md"
+              onClick={() => fetchData(filterData)}
             >
               Apply
             </button>
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,12 +1,12 @@
-'use client';
-import { Button, Dropdown, Modal, Table } from 'flowbite-react';
-import React, { useState, useEffect } from 'react';
-import Header from '../components/Header';
-import { Pagination } from 'flowbite-react';
-import FilterDropdown from '../components/FilterDropDown';
-import { FaArrowDown, FaArrowUp } from 'react-icons/fa';
-import ShimmerTable from '../components/ShimmerTable';
-import ShimmerLoader from '../components/ShimmerLoader';
+"use client";
+import { Button, Dropdown, Modal, Table } from "flowbite-react";
+import React, { useState, useEffect } from "react";
+import Header from "../components/Header";
+import { Pagination } from "flowbite-react";
+import FilterDropdown from "../components/FilterDropDown";
+import { FaArrowDown, FaArrowUp } from "react-icons/fa";
+import ShimmerTable from "../components/ShimmerTable";
+import ShimmerLoader from "../components/ShimmerLoader";
 interface Athlete {
   age: number;
   citizen: boolean;
@@ -21,182 +21,127 @@ interface Athlete {
 }
 
 const DashboardPage: React.FC = () => {
-  const [pageNumber,setPageNumber] = useState(0)
+  const [pageNumber, setPageNumber] = useState(0);
+  const [filterData, setfilterData] = useState({
+    ageRange: [18, 65],
+    gender: "all",
+    citizen: "all",
+  });
   const [ageRange, setAgeRange] = useState([18, 65]);
-  const [gender, setGender] = useState('all');
-  const [citizen, setCitizen] = useState('all');
-  const [isGenderSortedClicked,setIsGenderSortedClicked] = useState(false)
-  const [isAgeSortedClicked, setIsAgeSortedClicked] = useState(false)
-  const [isCitizenSortedClicked, setIsCitizenSortedClicked] = useState(false)
-  const [sortedData,setSortedData] = useState<Athlete[]>([]);
+  const [gender, setGender] = useState("all");
+  const [citizen, setCitizen] = useState("all");
+  const [columnSort, setcolumnSort] = useState("");
+  const [isGenderSortedClicked, setIsGenderSortedClicked] = useState(false);
+  const [isAgeSortedClicked, setIsAgeSortedClicked] = useState(false);
+  const [isCitizenSortedClicked, setIsCitizenSortedClicked] = useState(false);
+  const [sortedData, setSortedData] = useState<Athlete[]>([]);
   const [data, setData] = useState<Athlete[]>([]);
   const [openModal, setOpenModal] = useState<string | undefined>();
   const [selectedAthlete, setSelectedAthlete] = useState<Athlete | null>(null);
 
-    async function fetchData() {
-      console.log("###############",pageNumber)
-      const url = new URL(process.env.NEXT_PUBLIC_API_URL+'/athletes');
-        url.searchParams.append('page',pageNumber.toString())
-      
-
-      try {
-        const response = await fetch(url.toString()); // Replace with your server's actual URL
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        const fetchedData = await response.json();
-        if(pageNumber===0){
-
-          setData(fetchedData);
-          setSortedData(fetchedData);
-        }
-        else{
-
-          setData((prevData) => [...prevData, ...fetchedData]);
-          setSortedData((prevData) => [...prevData, ...fetchedData]);
-        }
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
+  const fetchAthleteData = async () => {
+    console.log("#####", filterData);
+    const url = new URL(process.env.NEXT_PUBLIC_API_URL + "/athletes");
+    if (filterData && filterData.ageRange[0]) {
+      url.searchParams.append("minAge", filterData.ageRange[0].toString());
+      url.searchParams.append("maxAge", filterData.ageRange[1].toString());
+      url.searchParams.append("gender", filterData.gender);
+      url.searchParams.append("citizen", filterData.citizen);
     }
 
-useEffect(() => {
-    fetchData();
-  }, [pageNumber]);
-  const handleRowClick = (athlete: Athlete) => {
-    setSelectedAthlete(athlete);
-    setOpenModal('dismissible');
-  };
-
-  const fetchAthleteData = (filterData:any)=>{
-    console.log("#####",filterData)
-    const url = new URL(process.env.NEXT_PUBLIC_API_URL+'/athletes');
-    if(filterData && filterData.ageRange[0]){
-
-    url.searchParams.append('minAge', filterData.ageRange[0].toString());
-    url.searchParams.append('maxAge', filterData.ageRange[1].toString());
-    url.searchParams.append('gender', filterData.gender);
-    url.searchParams.append('citizen', filterData.citizen);
-    url.searchParams.append('page',pageNumber.toString())
-  
-    }
-    fetch(url.toString(), {
-      method: 'GET',
+    url.searchParams.append("page", pageNumber.toString());
+    await fetch(url.toString(), {
+      method: "GET",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
     })
       .then((response) => {
         if (response.ok) {
-          return response.json(); 
+          return response.json();
         } else {
-          console.error('Error while making the GET request');
-          throw new Error('Network response was not ok');
+          console.error("Error while making the GET request");
+          throw new Error("Network response was not ok");
         }
       })
       .then((data) => {
-        
-        console.log(data)
-         setData(data);
-         
+        if (pageNumber != 0) {
+          setData((prevData) => [...prevData, ...data]);
+        } else {
+          setData(data);
+        }
       })
       .catch((error) => {
-        console.error('An error occurred:', error);
+        console.error("An error occurred:", error);
       });
-
-
-  }
-
-  const handleSort = () => {
-    const sortedData = [...data].sort((a: Athlete | undefined, b: Athlete | undefined) => {
-      if (a && b) {
-        if (isAgeSortedClicked) {
-          return a.age - b.age;
-        } else {
-          return b.age - a.age;
-        }
-      }
-      return 0;
-    });
-  
-    setData(sortedData);
-    setIsAgeSortedClicked(!isAgeSortedClicked);
   };
-  
-  const handleSortByCitizenship = () => {
-    const sortedData = [...data].sort((a: Athlete | undefined, b: Athlete | undefined) => {
-      if (a && b) {
-        if (isCitizenSortedClicked) {
-          return a.citizen === b.citizen ? 0 : a.citizen ? -1 : 1;
-        } else {
-          return a.citizen === b.citizen ? 0 : a.citizen ? 1 : -1;
-        }
-      }
-      return 0;
-    });
-  
-    setData(sortedData);
-    setIsCitizenSortedClicked(!isCitizenSortedClicked);
+
+  useEffect(() => {
+    console.log("RUNNING", pageNumber);
+    fetchAthleteData();
+  }, [pageNumber]);
+
+  const handleRowClick = (athlete: Athlete) => {
+    setSelectedAthlete(athlete);
+    setOpenModal("dismissible");
   };
-  const handleSortByGender = () => {
-    const sortedData = [...data].sort((a: Athlete | undefined, b: Athlete | undefined) => {
-      if (a && b) {
-        if (isGenderSortedClicked) {
-          return a.gender.localeCompare(b.gender);
-        } else {
-          return b.gender.localeCompare(a.gender);
-        }
-      }
-      return 0;
-    });
-  
-    setData(sortedData);
-    setIsGenderSortedClicked(!isGenderSortedClicked);
+
+  const updateSort = () => {};
+  const applyFilters = (filtered: Athlete[]) => {
+    setSortedData(filtered);
   };
-    
-  const applyFilters = (filtered:Athlete[])=>{
-    setSortedData(filtered)
-  }
   return (
     <div>
-      
-      <div className='p-4 flex justify-start items-center mr-8 space-x-8'>
-        <FilterDropdown fetchData={fetchAthleteData}/>
+      <div className="p-4 flex justify-start items-center mr-8 space-x-8">
+        <FilterDropdown
+          fetchData={() => setPageNumber(0)}
+          filterData={filterData}
+          setFilterData={setfilterData}
+        />
       </div>
       {data.length > 0 ? (
         <Table>
           <Table.Head>
             <Table.HeadCell>First Name</Table.HeadCell>
             <Table.HeadCell>Last Name</Table.HeadCell>
-            <Table.HeadCell onClick={()=>handleSort()} className=''><span className='cursor-pointer'>Age</span></Table.HeadCell>
-            <Table.HeadCell className='cursor-pointer' onClick={()=>handleSortByGender()}>Gender </Table.HeadCell>
+            <Table.HeadCell onClick={() => updateSort()} className="">
+              <span className="cursor-pointer">Age</span>
+            </Table.HeadCell>
+            <Table.HeadCell
+              className="cursor-pointer"
+              onClick={() => updateSort()}
+            >
+              Gender{" "}
+            </Table.HeadCell>
             <Table.HeadCell>College</Table.HeadCell>
             <Table.HeadCell>Instagram</Table.HeadCell>
-            <Table.HeadCell onClick={()=>handleSortByCitizenship()} className=''><span className='cursor-pointer'>Citizen</span></Table.HeadCell>
+            <Table.HeadCell onClick={() => updateSort()} className="">
+              <span className="cursor-pointer">Citizen</span>
+            </Table.HeadCell>
           </Table.Head>
-          <Table.Body className='divide-y'>
+          <Table.Body className="divide-y">
             {data.map((item, index) => (
               <Table.Row
                 key={index}
                 className={
                   index % 2 === 0
-                    ? 'bg-white cursor-pointer dark:border-gray-700 dark:bg-gray-800'
-                    : 'bg-gray-50 cursor-pointer dark:border-gray-700 dark:bg-gray-800'
+                    ? "bg-white cursor-pointer dark:border-gray-700 dark:bg-gray-800"
+                    : "bg-gray-50 cursor-pointer dark:border-gray-700 dark:bg-gray-800"
                 }
-                onClick={()=>handleRowClick(item)}
-
+                onClick={() => handleRowClick(item)}
               >
-                <Table.Cell className='whitespace-nowrap font-medium text-gray-900 dark:text:white'>
+                <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text:white">
                   {item.firstName}
                 </Table.Cell>
                 <Table.Cell>{item.lastName}</Table.Cell>
                 <Table.Cell>{item.age}</Table.Cell>
                 <Table.Cell>
-                  {item.gender.charAt(0).toUpperCase() + item.gender.slice(1).toLowerCase()}
+                  {item.gender.charAt(0).toUpperCase() +
+                    item.gender.slice(1).toLowerCase()}
                 </Table.Cell>
                 <Table.Cell>{item.college}</Table.Cell>
                 <Table.Cell>{item.instagram}</Table.Cell>
-                <Table.Cell>{item.citizen ? 'Yes' : 'No'}</Table.Cell>
+                <Table.Cell>{item.citizen ? "Yes" : "No"}</Table.Cell>
               </Table.Row>
             ))}
           </Table.Body>
@@ -204,26 +149,34 @@ useEffect(() => {
       ) : (
         <ShimmerLoader />
       )}
-      <div className='w-full flex justify-center items-center p-2'>
-
-      <button className='p-2 mt-10 bg-cyan-700 text-white font-semibold rounded-lg hover:bg-cyan-600 ease-in-out' onClick={()=>{setPageNumber(pageNumber+1)}}>Load More</button>
- 
+      <div className="w-full flex justify-center items-center p-2">
+        <button
+          className="p-2 mt-10 bg-cyan-700 text-white font-semibold rounded-lg hover:bg-cyan-600 ease-in-out"
+          onClick={() => {
+            setPageNumber(pageNumber + 1);
+          }}
+        >
+          Load More
+        </button>
       </div>
-      <Modal dismissible show={openModal === 'dismissible'} onClose={() => setOpenModal(undefined)}>
+      <Modal
+        dismissible
+        show={openModal === "dismissible"}
+        onClose={() => setOpenModal(undefined)}
+      >
         <Modal.Body>
-        {selectedAthlete && (
-          <div>
-            <h2>{`${selectedAthlete.firstName} ${selectedAthlete.lastName}`}</h2>
-            <p>{`Age: ${selectedAthlete.age}`}</p>
-            <p>{`Gender: ${selectedAthlete.gender}`}</p>
-            <p>{`College: ${selectedAthlete.college}`}</p>
-            <p>{`Instagram: ${selectedAthlete.instagram}`}</p>
-            <p>{`Citizen: ${selectedAthlete.citizen ? 'Yes' : 'No'}`}</p>
-          </div>
-        )}
+          {selectedAthlete && (
+            <div>
+              <h2>{`${selectedAthlete.firstName} ${selectedAthlete.lastName}`}</h2>
+              <p>{`Age: ${selectedAthlete.age}`}</p>
+              <p>{`Gender: ${selectedAthlete.gender}`}</p>
+              <p>{`College: ${selectedAthlete.college}`}</p>
+              <p>{`Instagram: ${selectedAthlete.instagram}`}</p>
+              <p>{`Citizen: ${selectedAthlete.citizen ? "Yes" : "No"}`}</p>
+            </div>
+          )}
         </Modal.Body>
       </Modal>
-      
     </div>
   );
 };
